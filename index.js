cb16b29d64fe89686af325eafe9c830be88bb1ed
Donna Amos <donnacamos.developer@gmail.com>
2019-09-26 03:47:50 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,25 +1,62 @@
-const baseURL = 'https://api.github.com';
-const user = '<YOUR_USERNAME>';
+const baseApi = 'https://api.github.com'
+const repo = 'javascript-fetch-lab'
+
+function getIssues() {
+  const url = `${baseApi}/repos/Donnadieu/${repo}/issues`
+
+  fetch(url, {
+    headers: {
+      Authorization: `token ${getToken()}`
+    }
+  }).then(res => res.json()).then(res => showIssues(res))
+}
+
+function showIssues(issues) {
+  document.getElementById('issues').append(renderIssues(issues))
+}
+
+const renderIssues = (issues) => (
+  "<ul>" + issues.map(issue => {
+    return(`
+        <li>
+          <h2>${issue.title}</h2>
+          <p>${issue.body}</p>
+        </li>
+      `)
+  }).join('') + "</ul>"
+)
+
+function createIssue() {
+  const issueTitle = document.getElementById('title').value
+  const issueBody = document.getElementById('body').value
+  const postData = { title: issueTitle, body: issueBody }
+  const url = `${baseApi}/repos/Donnadieu/${repo}/issues`
+
+  fetch(url, {
+    method: 'post',
+    headers: {
+      Authorization: `token ${getToken()}`
+    },
+    body: JSON.stringify(postData)
+  }).then(res => res.json()).then(getIssues())
+}
+
+function showForkedRepo(repo) {
+  $('#results').append(renderRepo(repo))
+}
+
+function forkRepo() {
+  fetch(`${baseApi}/repos/learn-co-curriculum/${repo}/forks`, {
+    method: 'post',
+    headers: {
+      Authorization: `token ${getToken()}`
+    }
+  }).then(res => res.json()).then(res => showForkedRepo(res))
+
+}
 
 function getToken() {
   //change to your token to run in browser, but set
   //back to '' before committing so all tests pass
-  return '';
-}
-
-function forkRepo() {
-  const repo = 'learn-co-curriculum/js-ajax-fetch-lab';
-  //use fetch to fork it!
-}
-
-function showResults(json) {
-  //use this function to display the results from forking via the API
-}
-
-function createIssue() {
-  //use this function to create an issue based on the values input in index.html
-}
-
-function getIssues() {
-  //once an issue is submitted, fetch all open issues to see the issues you are creating
+  return ''
 }
